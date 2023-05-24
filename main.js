a606c67d399a3c63831e590fd3ca3df6a824877c
blazecx <kirillr648@gmail.com>
2023-05-24 22:19:08 +0300

    Spirit

---
+++ main.js
@@ -0,0 +1,27 @@
+const tabsBtn = document.querySelectorAll(".nav_effect");
+const tabsItems = document.querySelectorAll(".tab_item");
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+  item.addEventListener("click", function () {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+
+    if (!currentBtn.classList.contains("active")) {
+      tabsBtn.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      tabsItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      currentBtn.classList.add("active");
+      currentTab.classList.add("active");
+    }
+  });
+}
+
+document.querySelector(".tabs__nav-btn").click();
