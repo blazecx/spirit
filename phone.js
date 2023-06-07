6155042d1c1da9d48a044af2e2adb06081c45208
blazecx <kirillr648@gmail.com>
2023-06-07 20:41:21 +0300

    Spirit

--- phone.js
+++ phone.js
@@ -1,6 +1,5 @@
-
-var element = document.getElementById('number');
+var element = document.getElementById("number");
 var maskOptions = {
-  mask: '+{7}(000)000-00-00'
+  mask: "+{7}(000)000-00-00",
 };
 var mask = IMask(element, maskOptions);
